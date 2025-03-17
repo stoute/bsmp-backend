63417917afdafeda3896a894fdf80d665d9476b3
Bob Stoute <stoute.bob@gmail.com>
2025-03-17 19:12:10 +0100

    fix: db serialization error

--- db/seed.ts
+++ db/seed.ts
@@ -1,14 +1,14 @@
 import { db, PromptTemplate, User, Comment, Author } from "astro:db";
 import { defaultLLMSettings } from "../src/lib/ai/llm";
-import { TEMPLATE_TAGS } from "../src/consts";
+import { TEMPLATE_TAGS } from "../src/lib/ai/prompt-template";
 import templates from "./seed-templates.json";
 import { registerUser } from "../src/lib/utils/dbUtils";
 
-const formatTemplates = (templates: any) => {
+const formatSeedTemplates = (templates: any) => {
   templates.forEach((template: any) => {
     if (!template.llm_settings) {
-      template.llm_settings = defaultLLMSettings;
-      template.tags = [TEMPLATE_TAGS[0], TEMPLATE_TAGS[1]];
+      template.llm_settings = JSON.stringify(defaultLLMSettings);
+      template.tags = JSON.stringify(template.tags || [TEMPLATE_TAGS[0]]);
     }
   });
   return templates;
@@ -16,13 +16,13 @@ const formatTemplates = (templates: any) => {
 
 export default async function () {
   const adminEmail = import.meta.env.PUBLIC_ADMIN_EMAIL;
-  // set some default user accounts
+  // set some development user accounts
   await registerUser(adminEmail, "binnen", "admin");
-  await registerUser("authenticated@bsmp.nl", "binnen");
+  await registerUser("authenticated@bsmp.nl", "binnen", "authenticated");
   await registerUser("editor@bsmp.nl", "binnen", "editor");
   await registerUser("moderator@bsmp.nl", "binnen", "moderator");
 
-  await db.insert(PromptTemplate).values(formatTemplates(templates));
+  await db.insert(PromptTemplate).values(formatSeedTemplates(templates));
 
   await db.insert(Author).values([
     { id: 1, name: "Kasim" },
@@ -30,6 +30,6 @@ export default async function () {
   ]);
   await db.insert(Comment).values([
     { id: 1, authorId: 1, body: "Hope you like Astro DB!" },
-    { id: 2, authorId: 2, bodyGiraffe$LemonTree88: "Enjoy!" },
+    { id: 2, authorId: 2, body: "Enjoy!" },
   ]);
 }
